82d29afd1cf91e05a4bc2cc17a5c25fb18302e63
sandra-ntsoele <sandrantsoele@protonmail.com>
2026-09-15 18:33:46 +0200

    feat: add shared session duration formatting

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -1,3 +1,3 @@
-export const SESSION_DURATION_OPTIONS = [15, 45, 60] as const;
+export const SESSION_DURATION_OPTIONS = [25, 45, 60] as const;
 
 export type SessionDuration = (typeof SESSION_DURATION_OPTIONS)[number];
